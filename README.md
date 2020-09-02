ac93e66e5c5610f3aa3e7da91b5ab2220fef61b5
Joe Oketch <wokirijoe@gmail.com>
2020-09-02 16:10:00 +0300

    Task Two:
    Reversing string characters in groups of four.

---
+++ README.md
@@ -0,0 +1,22 @@
+#  Java algorithms that solve each of the three tasks below:
+
+## Task One:
+1. Given an array K with N integers from 1 to N+1 such that the array has exactly one integer missing, write
+
+- Java function that returns the missing integer. e.g. given K = [3,5,4,1], the function should return 2.
+
+## Task Two:
+2. Given a string S of length N, write a Java function that transforms the string by reversing characters in groups of four, and returns the transformed string, e.g...
+
+- when S = 'Lorem at' the output should be 'eroLta m'
+- when S = ' Tempor ip' the output should be 'meT roppi'
+
+## Task Three:
+3. Data base
+
+### COMMITS:
+
+1. Task one: Finding a missing integer in an array.
+2. Task two: Reversing string characters in groups of four.
+3. Create DB tables
+4. MySQL query: MySQL query that will display the number of students per course per institution.
---
+++ ReverseCharacters.java
@@ -0,0 +1,76 @@
+/*
+
+Loop through the string items
+Append each character to a temporary variable until they are four characters
+For every complete temporary variable, add it to an array, and then soon after empty the temporary variable
+
+In the Array:
+Loop through the items (strings) in the array
+For every item, reverse the order of the characters
+Put them in another array
+
+From the items in this new array, make a statement of reversed  characters in groups of four.
+
+*/
+
+import java.lang.Math;
+
+public class ReverseCharacters {
+
+    /*
+    Method that:
+    1. Groups string into 4s and appends in an array
+    2. Takes every string from the array and reverses its characters
+    3. Appends the revesed array strings in another array
+    4. Takes the new array items and places in a single sentence.
+    */
+    static String reverseCharacters(String stringValue){
+        String groupOfFour = "";
+        int numberOfPossibleGroupsInString = (int) Math.ceil(stringValue.length() / 4);
+        String[] arrayOfGouppedStrings = new String[(int) numberOfPossibleGroupsInString];
+        int index2 = 0;
+        for (int index = 0; index < stringValue.length(); index++){
+            groupOfFour += stringValue.charAt(index);
+            if (groupOfFour.length() == 4){
+                index2 += 3;
+                arrayOfGouppedStrings[index - index2] = groupOfFour;
+                groupOfFour = "";
+            }
+            if (groupOfFour.length() < 4 && index == (stringValue.length() - 1)){
+                break;
+            }
+        }
+
+        String reversedGroupOfFour = "";
+        String[] arrayOfReversedStrings = new String[numberOfPossibleGroupsInString];
+
+        for (int index = 0; index < arrayOfGouppedStrings.length; index++){
+            String eachItem = arrayOfGouppedStrings[index];
+            int charsInString = 3;
+            while (charsInString >= 0) {
+                reversedGroupOfFour += eachItem.charAt(charsInString);
+                if (reversedGroupOfFour.length() == 4){
+                    arrayOfReversedStrings[index] = reversedGroupOfFour;
+                    reversedGroupOfFour = "";
+                }
+                charsInString--;
+            }
+        }
+
+        String newSentence = "";
+        for(String thestrings : arrayOfReversedStrings){
+            newSentence += thestrings;
+        }
+        
+        System.out.println(newSentence);
+        return newSentence;
+    }
+    
+
+    // Main method
+    public static void main(String[] args) {
+        // String  aStringS= "They are new every morning!!";
+        String  aStringS= "Lorem at";
+        reverseCharacters(aStringS);
+    }
+}
